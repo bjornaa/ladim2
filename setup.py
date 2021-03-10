6c4dfd5d1675483ad09cba8667ea682f1bebe4c6
Ådlandsvik Bjørn <bjorn@imr.no>
2021-03-10 08:16:08 +0100

    Moved to setup.cfg

--- setup.py
+++ setup.py
@@ -1,14 +1,2 @@
 from setuptools import setup
-
-# from setuptools import setup
-
-setup(
-    name="LADiM2",
-    version="2",
-    description="Lagrangian Advection and Diffusion Model",
-    author="Bjørn Ådlandsvik",
-    author_email="bjorn@imr.no",
-    packages=["ladim2"],
-    entry_points={'console_scripts': ['ladim2=ladim2.main:script']},
-    install_requires=["numpy", "pyyaml", "netCDF4", "pandas"],
-)
+setup()
