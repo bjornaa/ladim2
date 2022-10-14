9e9bc6de79666ae60f2bf9451e94058fc3343231
Bjørn Ådlandsvik <bjorn@hi.no>
2022-10-14 14:58:39 +0200

    Made test/test.grid.py working under vs code

--- test/test_grid.py
+++ test/test_grid.py
@@ -1,9 +1,14 @@
+from pathlib import Path
+
 from ladim.model import init_module
+
+grid_module = Path(__file__).parent / "grid0.py"
 
 def test_local():
     """Test the use of a local plug-in grid module"""
-
-    config = dict(module="grid0")
+    
+    # module argument must be a string (could be improved)
+    config = dict(module=str(grid_module))
     g = init_module('grid', config, dict())
     assert g.metric(10, 20) == 1
-    assert g.ingrid(10, 20) is True
+    assert g.ingrid(10, 20)
